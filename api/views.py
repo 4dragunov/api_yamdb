914d9275c776654a5ac0d5fba54afc4568280ad1
Alexey Sukhachevskiy <alyoshasu@yandex.ru>
2020-10-21 19:22:18 +0300

    Review and Comment models are done, Views and Routes in urls.py, serializers and permissions (api app) are prepared. Rating to be added.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from titles import Title
+from .serializers import ReviewSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
+from rest_framework import permissions
 
-# Create your views here.
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        title_id = self.kwargs['title_id']
+        return get_object_or_404(Title, pk=title_id).reviews
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        title_id = self.kwargs['title_id']
+        reviews_set = get_object_or_404(Title, pk=title_id).reviews
+        review_id = self.kwargs['review_id']
+        return get_object_or_404(reviews_set, pk=review_id).comments
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs['review_id']
+        author = self.request.user
+        post = get_object_or_404(Title, pk=post_id)
+        serializer.save(author=author, post=post)
---
+++ reviews/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from users import User
+from titles import Title
+
+
+class Review(models.Model):
+    text = models.TextField(verbose_name="Текст")
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата публикации",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        verbose_name="Автор",
+    )
+
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        verbose_name="Произведение",
+    )
+
+    class Meta:
+        ordering = ["-pub_date"]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Комментарий",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Пользователь",
+    )
+    text = models.TextField(verbose_name="Текст комментария")
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата комментария",
+    )
